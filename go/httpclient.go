85bc630ab6001c24e9b4294205e0fbbea26b21cd
iinux <iinux@qq.com>
2017-06-11 19:39:21 +0800

    http client

---
+++ go/httpclient.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"net/url"
+	"encoding/json"
+)
+
+func httpGet() {
+	resp, err := http.Get("http://www.01happy.com/demo/accept.php?id=1")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+func httpPost() {
+	resp, err := http.Post("http://www.01happy.com/demo/accept.php",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("name=cjb"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+func httpPostForm() {
+	resp, err := http.PostForm("http://www.01happy.com/demo/accept.php",
+		url.Values{"key": {"Value"}, "id": {"123"}})
+
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+
+}
+
+func httpDo() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://www.01happy.com/demo/accept.php", strings.NewReader("name=cjb"))
+	if err != nil {
+		// handle error
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+type ShiftList struct {
+	Code int
+	Message string
+	Timestamp int
+}
+
+func httpGetJson() {
+	resp, err := http.Get("http://m.echengbus.com/v2/office/shift/list.json?city=%E5%8C%97%E4%BA%AC")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+	var s ShiftList
+	json.Unmarshal(body, &s)
+	fmt.Println(s)
+}
+
+func main()  {
+	httpGetJson()
+}
+
