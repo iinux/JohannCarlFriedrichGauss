df0cbd395c9591ca5a606c785b2327573eb55f01
iinux <iinux@qq.com>
2017-06-11 15:35:39 +0800

    json and xml format

--- go/web2/app.go
+++ go/web2/app.go
@@ -5,9 +5,20 @@ import (
 	"net/http"
 	"os"
 	"html/template"
+	"encoding/json"
+	"encoding/xml"
 	//for extracting service credentials from VCAP_SERVICES
 	//"github.com/cloudfoundry-community/go-cfenv"
 )
+
+type Profile struct {
+	Name string
+	Hobbies []string
+}
+type ProfileXml struct {
+	Name string
+	Hobbies []string `xml:"Hobbies>Hobby"`
+}
 
 const (
 	DEFAULT_PORT = "8080"
@@ -15,12 +26,37 @@ const (
 )
 
 var index = template.Must(template.ParseFiles(
-  "templates/_base.html",
-  "templates/index.html",
+	"templates/_base.html",
+	"templates/index.html",
 ))
 
 func helloworld(w http.ResponseWriter, req *http.Request) {
-  index.Execute(w, nil)
+	index.Execute(w, nil)
+}
+
+func jsonFormat(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//w.Write([]byte("OK"))
+	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+	js, err := json.Marshal(profile)
+	if (err != nil) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)
+	w.WriteHeader(200)
+}
+func xmlFormat(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/xml")
+	//w.Write([]byte("OK"))
+	profile := ProfileXml{"Alex", []string{"snowboarding", "programming"}}
+	x, err := xml.Marshal(profile)
+	if (err != nil) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(x)
+	w.WriteHeader(200)
 }
 
 func main() {
@@ -35,8 +71,10 @@ func main() {
 	}
 
 	http.HandleFunc("/", helloworld)
+	http.HandleFunc("/1.json", jsonFormat)
+	http.HandleFunc("/1.xml", xmlFormat)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	
+
 	log.Printf("Starting app on %+v:%+v\n", host, port)
-	http.ListenAndServe(host+":"+port, nil)
+	http.ListenAndServe(host + ":" + port, nil)
 }
