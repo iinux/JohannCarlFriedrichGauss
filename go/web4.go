dd2ceb08b7811b5104c9e65e68324c720132efcf
iinux <iinux@qq.com>
2017-06-14 22:13:28 +0800

    rest api

---
+++ go/web4.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are get user %s", uid)
+}
+
+func modifyUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are modify user %s", uid)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are delete user %s", uid)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// uid := r.FormValue("uid")
+	uid := ps.ByName("uid")
+	fmt.Fprintf(w, "you are add user %s", uid)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	router.GET("/user/:uid", getUser)
+	router.POST("/adduser/:uid", addUser)
+	router.DELETE("/deluser/:uid", deleteUser)
+	router.PUT("/moduser/:uid", modifyUser)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
