a07280edd3c5d544714f74d716e7116662895096
iinux <iinux@qq.com>
2017-06-18 20:12:10 +0800

    online chat version 0.01

--- go/online_chat/server.go
+++ go/online_chat/server.go
@@ -5,6 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"encoding/json"
+)
+
+type Login struct {
+	UserId int
+	Username string
+	Password string
+}
+
+type SendSms struct {
+	ReceiptUid int
+	Content string
+}
+
+type OnlineUser struct {
+	Uid int
+	MessageChannel chan string
+}
+
+var (
+	OnlineUsers []OnlineUser
 )
 
 func Echo(ws *websocket.Conn) {
@@ -12,20 +34,51 @@ func Echo(ws *websocket.Conn) {
 
 	for {
 		var reply string
-
 		if err = websocket.Message.Receive(ws, &reply); err != nil {
 			fmt.Println("Can't receive")
 			break
 		}
 
-		fmt.Println("Received back from client: " + reply)
+		var index = strings.Index(reply, ":")
+		var action = reply[:index]
+		var rawData = reply[index + 1:]
 
-		msg := "Received:  " + reply
-		fmt.Println("Sending to client: " + msg)
+		switch action {
+		case "login":
+			var data Login
+			json.Unmarshal([]byte(rawData), &data)
 
-		if err = websocket.Message.Send(ws, msg); err != nil {
-			fmt.Println("Can't send")
+			var user OnlineUser
+			user.Uid = data.UserId
+			user.MessageChannel = make(chan string)
+
+			OnlineUsers = append(OnlineUsers, user)
+
+			fmt.Println(data)
+			fmt.Println(OnlineUsers)
+
+			go func(){
+				for {
+					var msg string
+					msg = <- user.MessageChannel
+					if err = websocket.Message.Send(ws, msg); err != nil {
+						fmt.Println("Can't send")
+						break
+					}
+				}
+			}()
+		case "logout":
+			fmt.Println("logout")
 			break
+		case "send":
+			var data SendSms
+			json.Unmarshal([]byte(rawData), &data)
+
+			for _, onlineUser := range OnlineUsers {
+				if onlineUser.Uid == data.ReceiptUid {
+					onlineUser.MessageChannel <- data.Content
+				}
+			}
 		}
 	}
 }
