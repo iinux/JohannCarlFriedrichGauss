90c4be4d5d1dedc963a5effb5e0722c9b1fb7387
iinux <iinux@qq.com>
2017-10-11 14:22:39 +0800

    php ml and go rtree

---
+++ go/rtree.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/dhconnelly/rtreego"
+	"fmt"
+)
+
+func main() {
+	rt := rtreego.NewTree(2, 25, 50)
+	p1 := rtreego.Point{0.4, 0.5}
+	p2 := rtreego.Point{6.2, -3.4}
+
+	r1, _ := rtreego.NewRect(p1, []float64{1, 2})
+	r2, _ := rtreego.NewRect(p2, []float64{1.7, 2.7})
+
+	rt.Insert(&Thing{r1, "foo"})
+	rt.Insert(&Thing{r2, "bar"})
+
+	size := rt.Size()
+
+	fmt.Println(size)
+}
+
+type Thing struct {
+	where *rtreego.Rect
+	name string
+}
+
+func (t *Thing) Bounds() *rtreego.Rect {
+	return t.where
+}
---
+++ php_function/ml.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qzhang
+ * Date: 2017/10/11
+ * Time: 14:20
+ */
+
+require_once 'vendor/autoload.php';
+
+use Phpml\Classification\KNearestNeighbors;
+
+$samples = [[1, 3], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
+$labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+
+$classifier = new KNearestNeighbors();
+$classifier->train($samples, $labels);
+
+echo $classifier->predict([3, 2]);
+// return 'b'
