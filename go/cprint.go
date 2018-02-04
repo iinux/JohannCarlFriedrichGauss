312628154d2018f35b70dab58953aece33d3d520
iinux <iinux@qq.com>
2018-02-04 20:02:04 +0800

    cprint

--- go/cprint.go
+++ go/cprint.go
@@ -4,15 +4,12 @@ package main
 #include <stdio.h>
 #include <stdlib.h>
 */
-import "C"
 import (
 	"C"
 	"unsafe"
-	"fmt"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
 	cstr := C.CString("hello world")
 	C.puts(cstr)
 	C.free(unsafe.Pointer(cstr))
