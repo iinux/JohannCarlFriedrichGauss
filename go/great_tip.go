f34d542442bc27785cfa93367563166ff38fd62b
iinux <iinux@qq.com>
2019-05-10 14:55:10 +0800

    great_tip keyboard press count

--- go/great_tip.go
+++ go/great_tip.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"time"
 	"fmt"
-	"syscall"
-	"unsafe"
 	"math/rand"
 	"os"
 	"strconv"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
-func abort(funcName string, err error) {
-	panic(fmt.Sprintf("%s failed: %v", funcName, err))
-}
-
 var (
-	user32, _      = syscall.LoadLibrary("user32.dll")
-	messageBox, _  = syscall.GetProcAddress(user32, "MessageBoxW")
-	keybd_event, _ = syscall.GetProcAddress(user32, "keybd_event")
+	user32                  = windows.NewLazySystemDLL("user32.dll")
+	messageBox              = user32.NewProc("MessageBoxW")
+	procSetWindowsHookEx    = user32.NewProc("SetWindowsHookExA")
+	procCallNextHookEx      = user32.NewProc("CallNextHookEx")
+	procUnhookWindowsHookEx = user32.NewProc("UnhookWindowsHookEx")
+	procGetMessage          = user32.NewProc("GetMessageW")
+	keyboardHook            HHOOK
+	keyboardPressCount      = 0
 )
 
 const (
@@ -44,29 +47,36 @@ const (
 	MB_DEFBUTTON4 = 0x00000300
 
 	MB_SYSTEMMODAL = 0x00001000
+
+	WH_KEYBOARD_LL = 13
+	WH_KEYBOARD    = 2
+	WM_KEYDOWN     = 256
+	WM_SYSKEYDOWN  = 260
+	WM_KEYUP       = 257
+	WM_SYSKEYUP    = 261
+	WM_KEYFIRST    = 256
+	WM_KEYLAST     = 264
+	PM_NOREMOVE    = 0x000
+	PM_REMOVE      = 0x001
+	PM_NOYIELD     = 0x002
+	WM_LBUTTONDOWN = 513
+	WM_RBUTTONDOWN = 516
+	NULL           = 0
 )
 
 func MessageBox(caption, text string, style uintptr) (result int) {
-	var nargs uintptr = 4
-	ret, _, callErr := syscall.Syscall9(uintptr(messageBox),
-		nargs,
+	messageBox.Call(
 		0,
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(text))),
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
-		style,
-		0,
-		0,
-		0,
-		0,
-		0)
-	if callErr != 0 {
-		abort("Call MessageBox", callErr)
-	}
-	result = int(ret)
+		style)
 	return
 }
 
 func main() {
+	go PressCountStart()
+	go PressCountTip()
+
 	wantMinuteStr := "0"
 	var everySecond int64
 	everySecond = 60
@@ -108,4 +118,110 @@ func random(category int) string {
 	l := len(stringMap[category])
 	r := rand.Intn(l)
 	return stringMap[category][r]
+}
+
+type (
+	DWORD     uint32
+	WPARAM    uintptr
+	LPARAM    uintptr
+	LRESULT   uintptr
+	HANDLE    uintptr
+	HINSTANCE HANDLE
+	HHOOK     HANDLE
+	HWND      HANDLE
+)
+
+type HOOKPROC func(int, WPARAM, LPARAM) LRESULT
+
+type KBDLLHOOKSTRUCT struct {
+	VkCode      DWORD
+	ScanCode    DWORD
+	Flags       DWORD
+	Time        DWORD
+	DwExtraInfo uintptr
+}
+
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd162805.aspx
+type POINT struct {
+	X, Y int32
+}
+
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms644958.aspx
+type MSG struct {
+	Hwnd    HWND
+	Message uint32
+	WParam  uintptr
+	LParam  uintptr
+	Time    uint32
+	Pt      POINT
+}
+
+func SetWindowsHookEx(idHook int, lpfn HOOKPROC, hMod HINSTANCE, dwThreadId DWORD) HHOOK {
+	ret, _, _ := procSetWindowsHookEx.Call(
+		uintptr(idHook),
+		uintptr(syscall.NewCallback(lpfn)),
+		uintptr(hMod),
+		uintptr(dwThreadId),
+	)
+	return HHOOK(ret)
+}
+
+func CallNextHookEx(hhk HHOOK, nCode int, wParam WPARAM, lParam LPARAM) LRESULT {
+	ret, _, _ := procCallNextHookEx.Call(
+		uintptr(hhk),
+		uintptr(nCode),
+		uintptr(wParam),
+		uintptr(lParam),
+	)
+	return LRESULT(ret)
+}
+
+func UnhookWindowsHookEx(hhk HHOOK) bool {
+	ret, _, _ := procUnhookWindowsHookEx.Call(
+		uintptr(hhk),
+	)
+	return ret != 0
+}
+
+func GetMessage(msg *MSG, hwnd HWND, msgFilterMin uint32, msgFilterMax uint32) int {
+	ret, _, _ := procGetMessage.Call(
+		uintptr(unsafe.Pointer(msg)),
+		uintptr(hwnd),
+		uintptr(msgFilterMin),
+		uintptr(msgFilterMax))
+	return int(ret)
+}
+
+func PressCountStart() {
+	// defer user32.Release()
+	keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL,
+		(HOOKPROC)(func(nCode int, wparam WPARAM, lparam LPARAM) LRESULT {
+			if nCode == 0 && wparam == WM_KEYDOWN {
+				if false {
+					kbdstruct := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lparam))
+					code := byte(kbdstruct.VkCode)
+					fmt.Printf("key pressed: %q\n", code)
+				}
+				keyboardPressCount++
+			}
+			return CallNextHookEx(keyboardHook, nCode, wparam, lparam)
+		}), 0, 0)
+	var msg MSG
+	for GetMessage(&msg, 0, 0, 0) != 0 {
+
+	}
+
+	UnhookWindowsHookEx(keyboardHook)
+	keyboardHook = 0
+}
+
+func PressCountTip() {
+	tick := time.Tick(time.Minute)
+
+	for {
+		<-tick
+		pressStr := fmt.Sprintf("you have %d press in last one minute", keyboardPressCount)
+		MessageBox("Press Count Tip", pressStr, MB_OK|MB_SYSTEMMODAL)
+		keyboardPressCount = 0
+	}
 }
