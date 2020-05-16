3938edf61ab278c08d24ed9743f154709adacc15
iinux <iinux@qq.com>
2020-05-16 23:58:20 +0800

    nm support ipv6

--- c/nm.c
+++ c/nm.c
@@ -39,9 +39,10 @@ int main(int argc, char **argv)
     char *host = "127.0.0.1";
     int port = 8888;
     int interval = 1;
+    int isv6 = 0;
 
     int ch;
-    while ((ch = getopt(argc, argv, "h:p:i:m:")) != -1)
+    while ((ch = getopt(argc, argv, "h:p:i:m:6")) != -1)
     {
         switch (ch)
         {
@@ -56,6 +57,9 @@ int main(int argc, char **argv)
             break;
         case 'm':
             break;
+        case '6':
+            isv6 = 1;
+            break;
         case '?': // 输入未定义的选项, 都会将该选项的值变为 ?
             printf("unknown option \n");
             break;
@@ -66,32 +70,83 @@ int main(int argc, char **argv)
 
     printf("host:%s port:%d interval:%d\n", host, port, interval);
 
-    extern int h_errno;
-    struct hostent *h;
-    struct in_addr in;
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr)); //每个字节都用0填充
-    h = gethostbyname(host);
-    if (h == NULL)
+    struct sockaddr_in6 serv_addr6;
+    memset(&serv_addr6, 0, sizeof(serv_addr6)); //每个字节都用0填充
+
+    int sock;
+    int n;
+    if (isv6 == 1)
     {
-        printf("%s\n", hstrerror(h_errno));
-        exit(1);
+        struct addrinfo hints, *servinfo, *p;
+
+        memset(&hints, 0, sizeof hints);
+        // hints.ai_family = AF_UNSPEC; // 設定 AF_INET6 表示強迫使用 IPv6
+        hints.ai_family = AF_INET6;
+        hints.ai_socktype = SOCK_STREAM;
+        char buf[8];
+        sprintf((char *)&buf, "%d", port);
+        n = getaddrinfo(host, buf, &hints, &servinfo);
+        if (n != 0)
+        {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(n));
+            exit(1);
+        }
+        for (p = servinfo; p != NULL; p = p->ai_next)
+        {
+            char buf[64];
+            struct sockaddr_in6 *clnt_addr = (struct sockaddr_in6 *)p->ai_addr;
+            inet_ntop(p->ai_family, &clnt_addr->sin6_addr, buf, sizeof(buf));
+            printf("name:%s, length:%d, addrtype:%d, ip:%s\n", p->ai_canonname, p->ai_addrlen, p->ai_family, buf);
+
+            sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+            if (sock == -1)
+            {
+                perror("socket");
+                continue;
+            }
+
+            n = connect(sock, p->ai_addr, p->ai_addrlen);
+            if (n == -1)
+            {
+                close(sock);
+                perror("connect");
+                continue;
+            }
+            // printf("%d\n", __LINE__);
+
+            break; // if we get here, we must have connected successfully
+        }
     }
     else
     {
+        extern int h_errno;
+        struct hostent *h;
+        struct in_addr in;
+
+        h = gethostbyname(host);
+        // printf("%d\n",__LINE__);
+        if (h == NULL)
+        {
+            perror("gethostbyname");
+            // printf("gethostbyname %s\n", hstrerror(h_errno));
+            exit(1);
+        }
+
         memcpy(&serv_addr.sin_addr.s_addr, h->h_addr, 4);
         in.s_addr = serv_addr.sin_addr.s_addr;
         printf("name:%s, length:%d, addrtype:%d, ip:%s\n", h->h_name, h->h_length, h->h_addrtype, inet_ntoa(in));
+
+        //创建套接字
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        //向服务器（特定的IP和端口）发起请求
+        serv_addr.sin_family = AF_INET; //使用IPv4地址
+        // serv_addr.sin_addr.s_addr = inet_addr(host);
+        serv_addr.sin_port = htons(port); //端口
+        n = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     }
 
-    //创建套接字
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    //向服务器（特定的IP和端口）发起请求
-    serv_addr.sin_family = AF_INET; //使用IPv4地址
-    // serv_addr.sin_addr.s_addr = inet_addr(host);
-    serv_addr.sin_port = htons(port); //端口
-    int n;
-    n = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     if (n < 0)
     {
         perror("connect");
@@ -101,6 +156,7 @@ int main(int argc, char **argv)
     while (1)
     {
         sleep(interval);
+        // printf("%d\n", __LINE__);
 
         /*
         // sys/timeb.h
@@ -118,12 +174,14 @@ int main(int argc, char **argv)
             perror("write");
             break;
         }
+        // printf("%d\n", __LINE__);
         n = read(sock, buffer, sizeof(buffer) - 1);
         if (n < 0)
         {
             perror("read");
             break;
         }
+        // printf("%d\n", __LINE__);
         // printf("Message form server: %s\n", buffer);
 
         /*
