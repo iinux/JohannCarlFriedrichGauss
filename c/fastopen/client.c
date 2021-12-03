99323b8a779df2ac38768395d0fdab1234be1aec
iinux <iinux@qq.com>
2021-12-03 23:46:46 +0800

    tcp fast open

---
+++ c/fastopen/client.c
@@ -0,0 +1,34 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
+int main()
+{
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char *data = "Hello, tcp fast open";
+    int data_len = strlen(data);
+
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    server = gethostbyname("localhost");
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(6666);
+
+    // /usr/src/linux-headers-4.4.0-22/include/linux/socket.h:#define MSG_FASTOPEN	0x20000000	/* Send data in TCP SYN */
+
+    //  int len = sendto(sfd, data, data_len, 0x20000000/*MSG_FASTOPEN*/,
+
+    int len = sendto(sfd, data, data_len, MSG_FASTOPEN /*MSG_FASTOPEN*/,
+                     (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (errno != 0)
+    {
+        printf("error: %s\n", strerror(errno));
+    }
+    close(sfd);
+}
